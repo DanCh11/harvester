93b413a852b7076b700a81756b5de4f0fa77f886
DanCh11 <dan.chiriac1453@gmail.com>
2024-05-22 18:48:51 +0200

    implemented strategy pattern refactored creation of sentiment analysis created dominant topics extraction strategy

---
+++ harvester/services/pipelines/ml_algorithms/ml_appliances.py
@@ -0,0 +1,105 @@
+import pandas as pd
+
+from collections import Counter
+
+from gensim.corpora.dictionary import Dictionary
+from gensim.models.ldamodel import LdaModel
+from nltk.sentiment import SentimentIntensityAnalyzer
+
+from .abstract_strategy import MLStrategy
+
+
+class CreateSentimentAnalysisStrategy(MLStrategy):
+    def execute(self, dataset: pd.DataFrame, text_column: str) -> pd.DataFrame:
+        """
+            Creates sentiment analysis to given dataset and column name.
+            @param dataset: given dataset
+            @param text_column: textual column
+
+            @return: pre-processed dataset with sentiment analysis data
+        """
+        sentiments = dataset[text_column].apply(
+            lambda text: SentimentIntensityAnalyzer().polarity_scores(text)
+            if isinstance(text, str)
+            else {'neg': None, 'neu': None, 'pos': None, 'compound': None})
+
+        sentiment_df = pd.json_normalize(sentiments)
+
+        return pd.concat([dataset, sentiment_df], axis=1)
+
+
+class ExtractDominantTopicsStrategy(MLStrategy):
+    def __init__(self):
+        self.lda_model: LdaModel
+        self.corpus: list
+        self.id2word: Dictionary
+
+    def execute(self,
+                dataset: pd.DataFrame,
+                text_column: str,
+                num_topics: int,
+                minimum_probability: float = .8,
+                most_common_elements: int = 10) -> pd.DataFrame:
+        """
+            Extracts dominant topics from given dataset and column name.
+            @param dataset: given dataset
+            @param text_column: textual column
+            @param num_topics: the number of topics
+            @param minimum_probability: sets a threshold for the dominant topics
+            @param most_common_elements: most common elements
+
+            @return: dataframe with dominant topics
+        """
+        words = dataset[text_column].apply(lambda doc: doc.split()).tolist()
+        self._create_dictionary_and_corpus(words)
+        self._train_lda_model(num_topics=num_topics)
+        topics = self._extract_dominant_topics(minimum_probability=minimum_probability)
+
+        return self._summarize_topics(topics, num_topics, most_common_elements)
+
+    def _create_dictionary_and_corpus(self, words: list[list[str]]) -> None:
+        self.id2word = Dictionary(words)
+        self.corpus = [self.id2word.doc2bow(word) for word in words]
+
+    def _train_lda_model(self, num_topics: int, random_state: int = 42) -> None:
+        self.lda_model = LdaModel(corpus=self.corpus, id2word=self.id2word,
+                                  num_topics=num_topics, random_state=random_state)
+
+    def _extract_dominant_topics(self, minimum_probability: float) -> list:
+        """
+            Designed to extract the most dominant topic for each document in the corpus.
+            For each document (doc_bow), this function retrieves a list of topics
+            with their associated probabilities. The result is a list of tuples,
+            where each tuple contains a topic ID and its probability.
+            @param minimum_probability: sets a threshold for the dominant topics
+            @return:
+        """
+        probability_index: int = 1
+        topic_id_index: int = 0
+
+        return [
+            max(self.lda_model.get_document_topics(
+                doc_bow, minimum_probability=minimum_probability),
+                key=lambda topic: topic[probability_index], default=(None,))[topic_id_index]
+
+            for doc_bow in self.corpus
+        ]
+
+    def _summarize_topics(self, topics: list[int], num_words: int, most_common_elements: int) -> pd.DataFrame:
+        """
+        Summarizes the most common topics in the given list of topics.
+        @param topics: A list of topics identified for each document.
+        @param num_words: The number of words to display for each topic.
+        @param most_common_elements: The number of most common topics to summarize.
+
+        @return: A DataFrame summarizing the top topics, their counts, and their most significant words.
+        """
+        topics_count = Counter(topic for topic in topics if topic is not None)
+        top_n_topics = topics_count.most_common(most_common_elements)
+
+        result = [
+            (topic, count, [word for word, _ in self.lda_model.show_topic(topic, topn=num_words)])
+            for topic, count in top_n_topics
+        ]
+
+        return pd.DataFrame(result, columns=['Topic', 'Count', 'Words'])
--- harvester/tests/unit/test_ml_appliances.py
+++ harvester/tests/unit/test_ml_appliances.py
@@ -1,16 +1,29 @@
 
 import pandas as pd
 
-from harvester.services.pipelines.ml_appliances import create_sentiment_analysis
+from harvester.services.pipelines.ml_algorithms.ml_appliances import (CreateSentimentAnalysisStrategy,
+                                                                      ExtractDominantTopicsStrategy)
 
 
 def test_create_sentiment_analysis(mock_reviews):
-    sentiment_df = create_sentiment_analysis(dataset=mock_reviews, column_name='comment', batch_size=1)
-    expected_columns = ['sentiment', 'positive', 'negative', 'neutral']
+    sentiment_df = CreateSentimentAnalysisStrategy().execute(dataset=mock_reviews, text_column='comment')
+    expected_columns = ['neg', 'neu', 'pos', 'compound']
 
     assert pd.DataFrame == type(sentiment_df)
     assert all(column in sentiment_df.columns for column in expected_columns)
-    assert 'negative' == sentiment_df['sentiment'][0]
-    assert 0.03846367076039314 == sentiment_df['positive'][0]
-    assert 0.7865731716156006 == sentiment_df['negative'][0]
-    assert 0.17496313154697418 == sentiment_df['neutral'][0]
+    assert 0.0 == sentiment_df['neg'][0]
+    assert 1.0 == sentiment_df['neu'][0]
+    assert 0.0 == sentiment_df['pos'][0]
+    assert 0.0 == sentiment_df['compound'][0]
+
+
+def test_extract_dominant_topics(mock_reviews):
+    dominant_topics = ExtractDominantTopicsStrategy().execute(dataset=mock_reviews, text_column='comment',
+                                                              num_topics=10)
+
+    expected_columns = ['Topic', 'Count', 'Words']
+    assert pd.DataFrame == type(dominant_topics)
+    assert dominant_topics['Topic'][0] == 1
+    assert dominant_topics['Count'][0] == 1
+    assert dominant_topics['Words'][0] == ['Ich', 'das', 'Jahrzehnten', 'von', 'Toilettenpapier', 'seit', 'Aldi.',
+                                           'kaufe', 'anderen', 'umsteigen,']
